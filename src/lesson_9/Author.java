55f3d704ccdc6c7c7bd87f030819022313926588
ilyaPronin <silverbolt777@yandex.ru>
2022-09-01 01:10:30 +0300

    well done

---
+++ src/lesson_9/Author.java
@@ -0,0 +1,26 @@
+package lesson_9;
+
+public class Author {
+    private final String firstName;
+    private final String surName;
+
+    public Author(String firstName,String surName){
+        this.firstName = firstName;
+        this.surName = surName;
+
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getSurName(){
+        return surName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", surName='" + surName + '\'' +
+                '}';
+    }
+}
---
+++ src/lesson_9/Book.java
@@ -0,0 +1,34 @@
+package lesson_9;
+
+public class Book {
+    private final String bookName;
+    private int publisherYear;
+   private final Author authorName;
+
+    public Book(String bookName,int publisherYear,Author authorName){
+        this.bookName =  bookName;
+        this.publisherYear = publisherYear;
+        this.authorName = authorName;
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public Author getAuthorName(){
+        return authorName;
+    }
+    public int getPublisherYear(){
+        return publisherYear;
+    }
+    public void  setPublisherYear(int publisherYear){
+        this.publisherYear = publisherYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", publisherYear=" + publisherYear +
+                ", authorName=" + authorName +
+                '}';
+    }
+}
